a8115638d7e6b93954944960922a0c365e87598f
Jessen V. Bredeson <jessenbredeson@berkeley.edu>
2021-12-03 14:48:13 -0800

    Rename files; Fix bad path

--- source-data/Fig.2/Fig.2b/plot-Fig.2b.sh
+++ source-data/Fig.2/Fig.2b/plot-Fig.2b.sh
@@ -2,11 +2,11 @@
 
 CWD=$(dirname $0);
 
-Rscript $CWD/../../software/artisanal/src/plot-collinearity-index.R \
-	Fig.2b/Fig.2b.1.fg.txt \
-	Fig.2b/Fig.2b.2.bg.txt \
-	Fig.2b/Fig.2b.3.loc.txt \
-	Fig.2b/Fig.2b.3.loc.txt \
+Rscript $CWD/../../../software/artisanal/src/plot-collinearity-index.R \
+	Fig.2b/Fig.2b.fg.txt \
+	Fig.2b/Fig.2b.bg.txt \
+	Fig.2b/Fig.2b.loc.bed \
+	Fig.2b/Fig.2b.loc.bed \
 	'D. alata' \
 	'D. alata' \
 	Fig.2b \
